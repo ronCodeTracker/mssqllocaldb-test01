f64003a9adf83a159ff47c915fe7f03abeb593ed
Ronald K <36178896+ronCodeTracker@users.noreply.github.com>
2024-01-17 22:27:38 -0600

    organize mod

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import express from 'express';
 
 //import { default as sql } from 'msnodesqlv8';
 //import sql from 'msnodesqlv8';
-import sql from 'mssql';
+//import sql from 'mssql';
 
 
 
@@ -17,6 +17,32 @@ import sql from 'mssql';
 
 const app = express();
 
+
+
+
+var config2 = {
+    "server": "(localdb)\\localDBDemo",
+    "database": "notes",
+    "driver": "msnodesqlv8",
+    "options": {
+        "trustedConnection": true
+    }
+};
+
+
+
+
+sql.connect(config2, function (err) {
+    //if (err) debug(err);
+    if (err) console.log(err);
+    var request = new sql.Request();
+    request.query('select * from notes', function (err, recordset) {
+        if (err) console.log(err)
+        //debug(recordset);
+        console.log(recordset);
+    });
+
+});
 
 
 
